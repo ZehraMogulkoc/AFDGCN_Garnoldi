70c0c5bbdf03b8a4beedd1fc82ce25f9e325ac65
Zehra Moğulkoç <87859856+ZehraMogulkoc@users.noreply.github.com>
2024-07-02 22:22:10 +0300

    Update load_dataset.py

--- lib/load_dataset.py
+++ lib/load_dataset.py
@@ -4,7 +4,7 @@ import numpy as np
 def load_st_dataset(dataset):
     # output B, N, D
     if dataset == 'PEMS04':
-        data_path = os.path.join('/content/AFDGCN_Garnoldi/data/PEMS04/PEMS04.npz')
+        data_path = os.path.join('./data/PEMS04/PEMS04.npz')
         data = np.load(data_path)['data'][:, :, :1]  # only the first dimension, traffic flow data
     elif dataset == 'PEMS08':
         data_path = os.path.join('./data/PEMS08/PEMS08.npz')
@@ -14,6 +14,12 @@ def load_st_dataset(dataset):
         data = np.load(data_path)['data'][:, :, :1]  # only the first dimension, traffic flow data
     elif dataset == 'PEMS07':
         data_path = os.path.join('./data/PEMS07/PEMS07.npz')
+        data = np.load(data_path)['data'][:, :, :1]  # only the first dimension, traffic flow data
+    elif dataset == 'Kcetas':
+        data_path = os.path.join('./data/PEMS04/morning_only.npz')
+        data = np.load(data_path)['data'][:, :, :1] 
+    elif dataset == 'Konya':
+        data_path = os.path.join('./data/Konya/konya_kavşaklar.npz')
         data = np.load(data_path)['data'][:, :, :1]  # only the first dimension, traffic flow data
     else:
         raise ValueError
