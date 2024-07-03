138340898e2877134e9a6bfb395e9aff25d6605d
Zehra Moğulkoç <87859856+ZehraMogulkoc@users.noreply.github.com>
2024-07-03 12:34:58 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,11 +4,14 @@ import configparser
 # *****************************************  参数初始化配置 ****************************************** #
 Mode = 'train'
 DEBUG = 'True'
-DATASET = 'PEMS04'
+DATASET = 'Konya'
+#DATASET = 'PEMS04'
 #DEVICE = 'cuda:1'
 DEVICE = 'cpu'
 MODEL = 'AFDGCN'
-GRAPH = "/content/AFDGCN_Garnoldi/data/PEMS04/PEMS04.csv"
+GRAPH = "/content/AFDGCN_Garnoldi/data/Konya/konya_kavşaklar.csv"
+#GRAPH = "./data/PEMS04/conn_graph.csv"
+#GRAPH = "./data/PEMS04/PEMS04.csv"
 K = 0.1
 # FILENAME_ID = "./data/PEMS03/PEMS03.txt"
 FILENAME_ID = None
