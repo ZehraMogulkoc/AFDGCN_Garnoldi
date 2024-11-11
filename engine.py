28cfbac6c381f1ceae2e6d5b8a21625b3a148fdc
Zehra Moğulkoç <87859856+ZehraMogulkoc@users.noreply.github.com>
2024-11-11 13:16:19 +0300

    Update engine.py

--- engine.py
+++ engine.py
@@ -37,7 +37,7 @@ class Engine(object):
         self.logger = get_logger(args.log_dir, name=args.model, debug=args.debug)
         self.logger.info('Experiment log path in: {}'.format(args.log_dir))
          # Create a SummaryWriter for TensorBoard
-        self.writer = SummaryWriter(log_dir="/content/AFDGCN_Garnoldi/logs_garnoldi_all")
+        self.writer = SummaryWriter(log_dir="/content/AFDGCN_Garnoldi/logs_konya_şubat")
         
 
     
